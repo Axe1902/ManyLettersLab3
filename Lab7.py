10ab6c299447cf23364de0fdce668091751adec0
Axe1902 <nikita-bar@bk.ru>
2021-11-21 12:08:23 +0300

    Lab7

--- Lab7.py
+++ Lab7.py
@@ -1,22 +1,49 @@
 import Graph
 import Operations
+import sys
 
-n = int(input("Введите размер матрицы: "))
 
-graph = []
-# graph = [[0, 2, 3, 0, 0, 1],
-#          [2, 0, 0, 5, 0, 0],
-#          [3, 0, 0, 0, 1, 0],
-#          [0, 5, 0, 0, 0, 0],
-#          [0, 0, 1, 0, 0, 0],
-#          [1, 0, 0, 0, 0, 0]]
-line = []
-Graph.generator_w_matrix(graph, n, 8)
-Graph.print_matrix(graph, 0, n)
-Graph.generator_line(line, graph, n)
+if __name__ == '__main__':
+    print(sys.argv)
+    n = int(input("Введите размер матрицы: "))
+    if int(sys.argv[1]) == 1:  # Взвешенный
+        if int(sys.argv[2]) == 1:  # Ориентированный
+            graph = []
+            Graph.generator_w_orientation_matrix(graph, n, 3)
+            Graph.print_matrix(graph, 0, n)
 
-Dist = [1000 for _ in range(n)]
+            Dist = [1000 for _ in range(n)]
+            node = int(input("Введите номер вершины: "))
+            Operations.bfsd_matrix_real(graph, Dist, node - 1)
+            print()
 
-node = int(input("Введите номер вершины: "))
-Operations.bfsd_matrix_real(graph, Dist, node-1)
-print()
+        elif int(sys.argv[2]) == 2:  # Неориентированный
+            graph = []
+            Graph.generator_w_matrix(graph, n, 3)
+            Graph.print_matrix(graph, 0, n)
+
+            Dist = [1000 for _ in range(n)]
+            node = int(input("Введите номер вершины: "))
+            Operations.bfsd_matrix_real(graph, Dist, node - 1)
+            print()
+
+    elif int(sys.argv[1]) == 2:  # Невзвешенный
+        if int(sys.argv[2]) == 1:
+            graph = []
+            Graph.generator_w_orientation_matrix(graph, n, 1)
+            Graph.print_matrix(graph, 0, n)
+
+            Dist = [1000 for _ in range(n)]
+            node = int(input("Введите номер вершины: "))
+            Operations.bfsd_matrix_real(graph, Dist, node - 1)
+            print()
+
+        elif int(sys.argv[2]) == 2:
+            graph = []
+            Graph.generator_w_matrix(graph, n, 1)
+            Graph.print_matrix(graph, 0, n)
+
+            Dist = [1000 for _ in range(n)]
+            node = int(input("Введите номер вершины: "))
+            Operations.bfsd_matrix_real(graph, Dist, node - 1)
+            print()
